b77f9a8ac4255c4fbbeebcbb5abbef65f30cb139
eminfedar <eminfedar@gmail.com>
2017-07-06 10:23:17 +0300

    Black style theme for linux

--- widgetci/main.cpp
+++ widgetci/main.cpp
@@ -1,9 +1,45 @@
 #include "mainwindow.h"
+#include "runguard.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    // Prevent multiple instances
+    RunGuard guard( "some_random_key" );
+        if ( !guard.tryToRun() )
+            return 0;
+
+
     QApplication a(argc, argv);
+
+    // STYLING.
+    #ifdef Q_OS_LINUX || Q_OS_UNIX
+        // because in Windows, it's not looking like supposed to be.
+        // later can be fixed.
+        QString style(
+                    "QHeaderView::section, QHeaderView::section *{"
+                    "padding: 3px;"
+                    "}"
+                    ""
+                    "QTreeView::item{"
+                    "margin-left: 0px;"
+                    "padding: 3px;"
+                    "}"
+                    ""
+                    "QMenu::item:selected{"
+                    "background-color: rgb(0, 150, 0);"
+                    "}"
+                    ""
+                    "*{"
+                    "background: #292929;"
+                    "color: #FFFFFF;"
+                    "}"
+                    ""
+                    );
+        a.setStyleSheet(style);
+    #endif
+
+
     mainWindow w;
     w.show();
 
