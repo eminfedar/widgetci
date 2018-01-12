7c2e1d3c27699e126cf8da0ee8280bc69091de88
eminfedar <eminfedar@gmail.com>
2018-01-12 23:58:35 +0300

    [ADDED] Empty TS Template for translators.

---
+++ widgetci/translations/widgetci_empty.ts
@@ -0,0 +1,209 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>WWidget</name>
+    <message>
+        <location filename="../wwidget.cpp" line="167"/>
+        <source>Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wwidget.cpp" line="173"/>
+        <source>Reload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wwidget.cpp" line="179"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wwidget.cpp" line="187"/>
+        <source>Always on Most top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wwidget.cpp" line="195"/>
+        <source>Always on top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wwidget.cpp" line="203"/>
+        <source>Normal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wwidget.cpp" line="211"/>
+        <source>Always on bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wwidget.cpp" line="219"/>
+        <source>Lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wwidget.cpp" line="233"/>
+        <source>Z-Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>mainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Widgetci</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="34"/>
+        <source>Widgets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="44"/>
+        <source>Go to http://github.com/eminfedar/widgetci</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="47"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;http://github.com/eminfedar/widgetci&quot;&gt;&lt;span style=&quot; font-size:10pt; font-style:italic; text-decoration: underline; color:#00cc00;&quot;&gt;contribute on GitHub&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="78"/>
+        <source>Refresh the widgets list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="110"/>
+        <source>Open the widgets folder...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="86"/>
+        <source>
+
+Please try to fix the error and refresh the list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="92"/>
+        <source>An error occured</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="212"/>
+        <location filename="../mainwindow.cpp" line="279"/>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="217"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="222"/>
+        <source>Reload</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="228"/>
+        <source>Refresh the list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="233"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="240"/>
+        <source>Delete From Disk...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="242"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="242"/>
+        <source>The &apos;%1&apos; folder will be deleted from disk.
+
+Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="246"/>
+        <location filename="../mainwindow.cpp" line="253"/>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="246"/>
+        <source>The folder has been deleted successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="253"/>
+        <source>The folder couldn&apos;t be removed.
+Check the folder and the files may be using by another program.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="282"/>
+        <source>Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="293"/>
+        <source>Open Manager...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="294"/>
+        <source>Reload All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="295"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>wqmlfile</name>
+    <message>
+        <location filename="../wqmlfile.cpp" line="18"/>
+        <location filename="../wqmlfile.cpp" line="45"/>
+        <location filename="../wqmlfile.cpp" line="75"/>
+        <location filename="../wqmlfile.cpp" line="94"/>
+        <source>ERR: 1st parameter is empty or null.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wqmlfile.cpp" line="21"/>
+        <location filename="../wqmlfile.cpp" line="48"/>
+        <location filename="../wqmlfile.cpp" line="97"/>
+        <source>ERR: 2nd parameter is empty or null.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wqmlfile.cpp" line="32"/>
+        <location filename="../wqmlfile.cpp" line="62"/>
+        <location filename="../wqmlfile.cpp" line="81"/>
+        <location filename="../wqmlfile.cpp" line="103"/>
+        <source>ERR: Can&apos;t access the file
+(maybe some program using it):</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wqmlfile.cpp" line="51"/>
+        <source>ERR: 3rd parameter is empty or null.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
