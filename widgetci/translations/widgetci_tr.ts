4bd6553291ad51c5dd2ffa3a71f1d6ab1b8920c3
Emin Fedar <eminfedar@gmail.com>
2018-08-28 14:32:43 +0300

    [UPDATE] Turkish Translations.

--- widgetci/translations/widgetci_tr.ts
+++ widgetci/translations/widgetci_tr.ts
@@ -4,47 +4,51 @@
 <context>
     <name>WWidget</name>
     <message>
-        <location filename="wwidget.cpp" line="167"/>
+        <location filename="../wwidget.cpp" line="192"/>
         <source>Hide</source>
         <translation>Gizle</translation>
     </message>
     <message>
-        <location filename="wwidget.cpp" line="173"/>
+        <location filename="../wwidget.cpp" line="198"/>
+        <source>Move</source>
+        <translation>Taşı</translation>
+    </message>
+    <message>
+        <location filename="../wwidget.cpp" line="206"/>
         <source>Reload</source>
         <translation>Yenile</translation>
     </message>
     <message>
-        <location filename="wwidget.cpp" line="179"/>
+        <location filename="../wwidget.cpp" line="212"/>
         <source>Edit</source>
         <translation>Düzenle</translation>
     </message>
     <message>
-        <location filename="wwidget.cpp" line="187"/>
+        <location filename="../wwidget.cpp" line="220"/>
         <source>Always on Most top</source>
         <translation>Her zaman En Üstte</translation>
     </message>
     <message>
-        <location filename="wwidget.cpp" line="195"/>
+        <location filename="../wwidget.cpp" line="228"/>
         <source>Always on top</source>
         <translation>Her zaman Üstte</translation>
     </message>
     <message>
-        <location filename="wwidget.cpp" line="203"/>
+        <location filename="../wwidget.cpp" line="236"/>
         <source>Normal</source>
         <translation>Normal</translation>
     </message>
     <message>
-        <location filename="wwidget.cpp" line="211"/>
+        <location filename="../wwidget.cpp" line="244"/>
         <source>Always on bottom</source>
         <translation>Her zaman altta</translation>
     </message>
     <message>
-        <location filename="wwidget.cpp" line="219"/>
         <source>Lock</source>
-        <translation>Kilitle</translation>
+        <translation type="vanished">Kilitle</translation>
     </message>
     <message>
-        <location filename="wwidget.cpp" line="233"/>
+        <location filename="../wwidget.cpp" line="260"/>
         <source>Z-Position</source>
         <translation>Z-Pozisyon</translation>
     </message>
@@ -52,85 +56,95 @@
 <context>
     <name>mainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
+        <location filename="../mainwindow.ui" line="14"/>
         <source>Widgetci</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="34"/>
+        <location filename="../mainwindow.ui" line="34"/>
         <source>Widgets</source>
         <translation>Widgetler</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="44"/>
+        <location filename="../mainwindow.ui" line="44"/>
         <source>Go to http://github.com/eminfedar/widgetci</source>
         <translation>http://github.com/eminfedar/widgetci adresine gidin</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="47"/>
+        <location filename="../mainwindow.ui" line="47"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;http://github.com/eminfedar/widgetci&quot;&gt;&lt;span style=&quot; font-size:10pt; font-style:italic; text-decoration: underline; color:#00cc00;&quot;&gt;contribute on GitHub&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;http://github.com/eminfedar/widgetci&quot;&gt;&lt;span style=&quot; font-size:10pt; font-style:italic; text-decoration: underline; color:#00cc00;&quot;&gt;GıtHub adresine git&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="78"/>
+        <location filename="../mainwindow.ui" line="78"/>
         <source>Refresh the widgets list</source>
         <translation>Widget listesini Yenile</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="110"/>
+        <location filename="../mainwindow.ui" line="110"/>
         <source>Open the widgets folder...</source>
         <translation>Widgetlerin bulunduğu klasörü aç...</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="86"/>
         <source>
 
 Please try to fix the error and refresh the list.</source>
-        <translation>Lütfen hatayı çözün ve listeyi tekrardan yenileyin.</translation>
+        <translation type="vanished">
+
+Lütfen hatayı çözün ve listeyi tekrardan yenileyin.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="92"/>
+        <location filename="../mainwindow.cpp" line="88"/>
+        <source>
+
+Please try to fix the error and refresh the list</source>
+        <translation>
+
+Lütfen kodlardaki hatayı çözün ve widgeti tekrar yenileyin</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="94"/>
         <source>An error occured</source>
         <translation>Bir hata oluştur</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="212"/>
-        <location filename="mainwindow.cpp" line="279"/>
+        <location filename="../mainwindow.cpp" line="209"/>
+        <location filename="../mainwindow.cpp" line="276"/>
         <source>Show</source>
         <translation>Göster</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="217"/>
+        <location filename="../mainwindow.cpp" line="214"/>
         <source>Edit</source>
         <translation>Düzenle</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="222"/>
+        <location filename="../mainwindow.cpp" line="219"/>
         <source>Reload</source>
         <translation>Yenile</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="228"/>
+        <location filename="../mainwindow.cpp" line="225"/>
         <source>Refresh the list</source>
         <translation>Listeyi yenile</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="233"/>
+        <location filename="../mainwindow.cpp" line="230"/>
         <source>Remove</source>
         <translation>Kaldır</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="240"/>
+        <location filename="../mainwindow.cpp" line="237"/>
         <source>Delete From Disk...</source>
         <translation>Diskten sil...</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="242"/>
+        <location filename="../mainwindow.cpp" line="239"/>
         <source>Warning</source>
         <translation>Uyarı</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="242"/>
+        <location filename="../mainwindow.cpp" line="239"/>
         <source>The &apos;%1&apos; folder will be deleted from disk.
 
 Are you sure?</source>
@@ -139,40 +153,40 @@ Are you sure?</source>
 Emin misiniz?</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="246"/>
-        <location filename="mainwindow.cpp" line="253"/>
+        <location filename="../mainwindow.cpp" line="243"/>
+        <location filename="../mainwindow.cpp" line="250"/>
         <source>Information</source>
         <translation>Bilgi</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="246"/>
+        <location filename="../mainwindow.cpp" line="243"/>
         <source>The folder has been deleted successfully.</source>
         <translation>Klasör başarıyla silindi.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="253"/>
+        <location filename="../mainwindow.cpp" line="250"/>
         <source>The folder couldn&apos;t be removed.
 Check the folder and the files may be using by another program.</source>
         <translation>Klasör silinemedi.
 Klasörün veya dosyaların başka bir program tarafından kullanılmadığına emin olun.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="282"/>
+        <location filename="../mainwindow.cpp" line="279"/>
         <source>Hide</source>
         <translation>Gizle</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="293"/>
+        <location filename="../mainwindow.cpp" line="290"/>
         <source>Open Manager...</source>
         <translation>Yöneticiyi Aç...</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="294"/>
+        <location filename="../mainwindow.cpp" line="291"/>
         <source>Reload All</source>
         <translation>Hepsini Yenile</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="295"/>
+        <location filename="../mainwindow.cpp" line="292"/>
         <source>Quit</source>
         <translation>Çıkış</translation>
     </message>
@@ -180,34 +194,50 @@ Klasörün veya dosyaların başka bir program tarafından kullanılmadığına 
 <context>
     <name>wqmlfile</name>
     <message>
-        <location filename="wqmlfile.cpp" line="18"/>
-        <location filename="wqmlfile.cpp" line="45"/>
-        <location filename="wqmlfile.cpp" line="75"/>
-        <location filename="wqmlfile.cpp" line="94"/>
+        <location filename="../wqmlfile.cpp" line="18"/>
+        <location filename="../wqmlfile.cpp" line="45"/>
+        <location filename="../wqmlfile.cpp" line="75"/>
+        <location filename="../wqmlfile.cpp" line="94"/>
         <source>ERR: 1st parameter is empty or null.</source>
         <translation>HATA: 1. Parametre boş veya null.</translation>
     </message>
     <message>
-        <location filename="wqmlfile.cpp" line="21"/>
-        <location filename="wqmlfile.cpp" line="48"/>
-        <location filename="wqmlfile.cpp" line="97"/>
+        <location filename="../wqmlfile.cpp" line="21"/>
+        <location filename="../wqmlfile.cpp" line="48"/>
+        <location filename="../wqmlfile.cpp" line="97"/>
         <source>ERR: 2nd parameter is empty or null.</source>
         <translation>HATA: 2. Parametre boş veya null.</translation>
     </message>
     <message>
-        <location filename="wqmlfile.cpp" line="32"/>
-        <location filename="wqmlfile.cpp" line="62"/>
-        <location filename="wqmlfile.cpp" line="81"/>
-        <location filename="wqmlfile.cpp" line="103"/>
+        <location filename="../wqmlfile.cpp" line="32"/>
+        <location filename="../wqmlfile.cpp" line="62"/>
+        <location filename="../wqmlfile.cpp" line="81"/>
+        <location filename="../wqmlfile.cpp" line="103"/>
         <source>ERR: Can&apos;t access the file
 (maybe some program using it):</source>
         <translation>HATA: Dosyaya erişilemiyor
 (Bir programın dosyayı kullanmadığına emin olun):</translation>
     </message>
     <message>
-        <location filename="wqmlfile.cpp" line="51"/>
+        <location filename="../wqmlfile.cpp" line="51"/>
         <source>ERR: 3rd parameter is empty or null.</source>
         <translation>HATA: 3. Parametre boş veya null.</translation>
+    </message>
+</context>
+<context>
+    <name>wqmlsystem</name>
+    <message>
+        <location filename="../wqmlsystem.cpp" line="98"/>
+        <source>ERR: Can&apos;t access the file
+(maybe some program using it):</source>
+        <translation>HATA: Dosyaya erişilemiyor
+(Bir programın dosyayı kullanmadığına emin olun):</translation>
+    </message>
+    <message>
+        <location filename="../wqmlsystem.cpp" line="110"/>
+        <location filename="../wqmlsystem.cpp" line="113"/>
+        <source>Can&apos;t find the CPU Name</source>
+        <translation>CPU İsmi bulunamadı</translation>
     </message>
 </context>
 </TS>
